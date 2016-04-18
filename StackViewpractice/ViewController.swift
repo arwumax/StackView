56759bcb4fef12eb73c5d546c54b6671cf530d6c
arwumax31@gmail.com <arwumax31@gmai.com>
2016-04-18 17:56:16 +0800

    Initial Commit

---
+++ StackViewpractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewpractice
+//
+//  Created by WuKwok Ho on 18/4/2016.
+//  Copyright © 2016 Wu Kwok Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
